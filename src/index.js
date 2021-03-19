4fffdd8cd6e86a1f45e275fc80ab155626e15637
VijaySyajiv <66031125+VijaySyajiv@users.noreply.github.com>
2021-03-19 12:19:27 +0530

    main

--- src/index.js
+++ src/index.js
@@ -1,96 +1,94 @@
 import React, { useState } from "react";
+import Modal from 'react-modal';
 import ReactDOM from "react-dom";
 import Square from './square';
 import Restart from './restart';
 import "./index.css";
 
+
 import calculateWinner from './calculatewinner';
-
+Modal.setAppElement("#root")
 function Game() {
-  const [ squares, setSquares ] = useState(Array(9).fill(null));
-  const [ isXNext, setIsXNext ] = useState(true);
-  const nextSymbol = isXNext ? "X" : "O";
-  const winner = calculateWinner(squares);
+        const [ modalVisibility , setModalVisibility ] = useState(true);
+        const [ squares, setSquares ] = useState(Array(9).fill(null));
+        const [ isXNext, setIsXNext ] = useState(true);
+        const nextSymbol = isXNext ? "X" : "O";
+        const winner = calculateWinner(squares);
 
-  function getStatus() {
-    if (winner) {
-      {
-        document.querySelector('[data-winning-message-text]').innerText ="Winner is -"+winner;
-        document.getElementById('winningMessage').classList.add('show')
-      }
-      return "Winner: " + winner;
-    } else if (isBoardFull(squares)) {
-      {
-        document.querySelector('[data-winning-message-text]').innerText = "Draw ";
-        document.getElementById('winningMessage').classList.add('show')
-      }
-      return "Draw!";
-    } else {
-      return "Next player: " + nextSymbol;
-    }
-  }
-  function isBoardFull(squares) {
-    for (let i = 0; i < squares.length; i++) {
-      if (squares[i] == null) {
-        return false;
-      }
-    }
-    return true;
-  }
+        function getStatus() {
+                if (winner)
+                   return "Winner: " + winner;
+                
+                else if (isBoardFull(squares)) 
+                    return "Draw!";
+                
+                else 
+                    return "Next player: " + nextSymbol;
+                 
+        }
+        function isBoardFull(squares) 
+        {
+            for (let i = 0; i < squares.length; i++) 
+            {
+              if (squares[i] == null) {
+                return false;
+              }
+            }
+            return true;
+        }
 
-  function renderSquare(i) {
-    return (
-      <Square
-        value={squares[i]}
-        onClick={() => {
-          if (squares[i] != null || winner != null) {
-            return;
-          }
-          const nextSquares = squares.slice();
-          nextSquares[i] = nextSymbol;
-          setSquares(nextSquares);
+        function renderSquare(i)
+         {
+            return (
+              <Square
+                  value={squares[i]}
+                  onClick={() => {
+                      if (squares[i] != null || winner != null) {
+                        return;
+                    }
+                    const nextSquares = squares.slice();
+                    nextSquares[i] = nextSymbol;
+                    setSquares(nextSquares);
+                    setIsXNext(!isXNext); // toggle turns
+                  }}
+              />
+            );
+        }
 
-          setIsXNext(!isXNext); // toggle turns
-        }}
-      />
-    );
-  }
+        function renderRestartButton() 
+        {
+            return (
+              <Restart
+                onClick={() => {
+                  setSquares(Array(9).fill(null));
+                  setIsXNext(true);
+                  setModalVisibility(false);
+                }}
+              />
+            );
+        }
 
-  function renderRestartButton() {
-    return (
-      <Restart
-        onClick={() => {
-          setSquares(Array(9).fill(null));
-          setIsXNext(true);
-          {
-            document.getElementById('winningMessage').classList.remove('show')
-          }
-        }}
-      />
-    );
-  }
-
-  return (
-    <div className="game" id="board">
-          <div className="board">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)} 
-          </div>
-         <div className="game-info">{getStatus()}</div>
-        
-        <div class="winning-message" id="winningMessage">
-    <div data-winning-message-text></div>
-    < div className="restart-button">{renderRestartButton()}</div>
-  </div>
-    </div>
-  );
+        return (
+            <div className="game" id="board">
+                    <div className="board">
+                        {renderSquare(0)}
+                        {renderSquare(1)}
+                        {renderSquare(2)}
+                        {renderSquare(3)}
+                        {renderSquare(4)}
+                        {renderSquare(5)}
+                        {renderSquare(6)}
+                        {renderSquare(7)}
+                        {renderSquare(8)} 
+                    </div>
+                  <div className="game-info">{getStatus()}</div>
+                  <Modal className="winning-message" id="winningMessage"isOpen={(winner!==null?true:false)|| isBoardFull(squares)? true:false}>
+                            <div >{getStatus()}</div>
+                            < div className="restart-button">{renderRestartButton()}</div>
+                  </Modal>
+                
+            </div>
+        );
 }
 
 ReactDOM.render(<Game />, document.getElementById("root"));
